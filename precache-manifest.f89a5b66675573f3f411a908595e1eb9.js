e4b64508d1917671b0e20db22f863d1d82f07992
raulshma <raulshma@gmail.com>
2020-02-05 17:12:54 +0530

    Updates

---
+++ precache-manifest.f89a5b66675573f3f411a908595e1eb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5799180ff9a4939e019950e3a5b1d8",
+    "url": "/fetch-dogs/index.html"
+  },
+  {
+    "revision": "c047ed9e43d5b67b2e09",
+    "url": "/fetch-dogs/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "4eeef7cd803a5f006b13",
+    "url": "/fetch-dogs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c047ed9e43d5b67b2e09",
+    "url": "/fetch-dogs/static/js/2.d6df92e3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fetch-dogs/static/js/2.d6df92e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4eeef7cd803a5f006b13",
+    "url": "/fetch-dogs/static/js/main.c1aeec2e.chunk.js"
+  },
+  {
+    "revision": "6a6695dd2b9b79a7bb06",
+    "url": "/fetch-dogs/static/js/runtime-main.c920faec.js"
+  }
+]);
